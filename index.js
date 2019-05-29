7aeec7d4ab7604764de0384fd827ec8bfe8fa308
Eric <escohen98@gmail.com>
2019-05-28 17:30:50 -0700

    Good for noe.

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ data.sort(function(x, y) {
 // from http://bl.ocks.org/mbostock/4349187
 // Sample from a normal distribution with mean 0, stddev 1.
 function normal() {
-    var x = 0,
+    var x = 1,
         y = 0,
         rds, c;
     do {
