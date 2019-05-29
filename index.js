7b7b096a8090893dd156b3962e59ba704bf00aaf
escohen98 <escohen98@gmail.com>
2019-05-29 01:48:41 -0700

    Got areas to work, removed y-axis, though only 1 curve is showing up.

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 //Taken from http://bl.ocks.org/phil-pedruco/88cb8a51cdce45f13c7e
 //setting up empty data array
-var data = [];
+let data = [];
+let points = [];
 
 getData(); // popuate data
 
 // line chart based on http://bl.ocks.org/mbostock/3883245
-var margin = {
+let margin = {
         top: 20,
         right: 20,
         bottom: 30,
@@ -14,21 +15,21 @@ var margin = {
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-var x = d3.scale.linear()
+let x = d3.scale.linear()
     .range([0, width]);
 
-var y = d3.scale.linear()
+let y = d3.scale.linear()
     .range([height, 0]);
 
-var xAxis = d3.svg.axis()
+let xAxis = d3.svg.axis()
     .scale(x)
     .orient("bottom");
 
-var yAxis = d3.svg.axis()
+let yAxis = d3.svg.axis()
     .scale(y)
     .orient("left");
 
-var line = d3.svg.line()
+let line = d3.svg.line()
     .x(function(d) {
         return x(d.q);
     })
@@ -37,7 +38,7 @@ var line = d3.svg.line()
     });
 
 //Custom code
-var line2 = d3.svg.line()
+let line2 = d3.svg.line()
   .x(function(d) {
     return x(d.q);
   })
@@ -45,7 +46,19 @@ var line2 = d3.svg.line()
     return y(d.o);
   })
 //
-var svg = d3.select("body").append("svg")
+
+//Taken from http://bl.ocks.org/jacobw56/2fd529120462c8ee044bccc3b0836547
+let area = d3.svg.area()
+  .x(function(d) { if(x(d.q)>445) return x(d.q); return x(0);})
+  .y0(450)
+  .y1(function(d) { return y(d.p); });
+
+let area1 = d3.svg.area()
+  .x(function(d) { if(x(d.q)<=445) return x(d.q); return x(0);})
+  .y0(450)
+  .y1(function(d) { return y(d.p); });
+
+let svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
@@ -63,34 +76,65 @@ svg.append("g")
     .attr("transform", "translate(0," + height + ")")
     .call(xAxis);
 
-svg.append("g")
-    .attr("class", "y axis")
-    .call(yAxis);
+// svg.append("g")
+//     .attr("class", "y axis")
+//     .call(yAxis);
+
+svg.append("path")
+  .datum(data)
+  .attr("class", "area")
+  .attr("d", area);
+
+svg.append("path")
+  .datum(data)
+  .attr("class", "area1")
+  .attr("d", area1);
 
 svg.append("path")
     .datum(data)
     .attr("class", "line")
     .attr("d", line)
 
+
 //Custom code
 svg.append("path")
     .datum(data)
-    .attr("class", "line")
-    .attr("d", line2);
+    .attr("class", "line1")
+    .attr("d", line2)
+    .attr("d", area);
+
+//Taken from https://www.dashingd3js.com/svg-paths-and-d3js
+let lineFunction = d3.svg.line()
+  .x(function(d) { return d.x; })
+  .y(function(d) { return d.y; })
+  .interpolate("linear");
+
+
 
 function getData() {
 
 // loop to populate data array with
 // probabily - quantile pairs
-for (var i = 0; i < 100000; i++) {
-    q = normal() // calc random draw from normal dist
-    p = gaussian(q) // calc prob of rand draw
-    o = gaussian(q, 1) // calc prob of rand draw with mean = 1, custom code
+for (let i = 0; i < 100000; i++) {
+  /*y*/  q = normal() // calc random draw from normal dist
+  /*x*/  p = gaussian(q) // calc prob of rand draw
+  /*x*/  o = gaussian(q, 1) // calc prob of rand draw with mean = 1, custom code
     el = {
         "q": q,
         "p": p,
         "o": o
     }
+    //P is unshifted mean
+    //O is shifted mean
+    // if(p>0.5) {
+    //   console.log("This happened...")
+    //   let point = [{"x": p, "y": q}, {"x": p, "y": 0}];
+    //   svg.append("path")
+    //     .attr("d", linefunction(points))
+    //     .attr("stroke", "red")
+    //     .attr("stroke-width", 2)
+    //     .attr("fill", none);
+    // }
     data.push(el)
 };
 
@@ -99,12 +143,13 @@ for (var i = 0; i < 100000; i++) {
 data.sort(function(x, y) {
     return x.q - y.q;
 });
+console.log(data)
 }
 
 // from http://bl.ocks.org/mbostock/4349187
 // Sample from a normal distribution with mean 0, stddev 1.
 function normal() {
-    var x = 1,
+    let x = 1,
         y = 0,
         rds, c;
     do {
@@ -119,7 +164,7 @@ function normal() {
 //taken from Jason Davies science library
 // https://github.com/jasondavies/science.js/
 function gaussian(x, m=0) {
-	var gaussianConstant = 1 / Math.sqrt(2 * Math.PI),
+	let gaussianConstant = 1 / Math.sqrt(2 * Math.PI),
 		mean = m,
     	sigma = 1;
 
